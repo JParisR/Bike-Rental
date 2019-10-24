a7812fb27beabec7ec17f56205e1a9d386dff3b2
Miguel Misa <miguel.misa@udc.es>
2019-10-24 11:59:06 +0200

    Jdbc

---
+++ ws-app-model/src/main/java/es/udc/ws/bikes/model/bike/Jdbc3CcSqlBikeDao.java
@@ -0,0 +1,52 @@
+package es.udc.ws.bikes.model.bike;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+
+public class Jdbc3CcSqlBikeDao extends AbstractSqlBikeDao {
+
+    @Override
+    public Bike create(Connection connection, Bike bike) {
+
+        /* Create "queryString". */
+        String queryString = "INSERT INTO Bike"
+                + " (description, price, creationDate)"
+                + " VALUES (?, ?, ?)";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(
+                        queryString, Statement.RETURN_GENERATED_KEYS)) {
+
+            /* Fill "preparedStatement". */
+            int i = 1;
+            preparedStatement.setString(i++, bike.getDescription());
+            preparedStatement.setFloat(i++, bike.getPrice());
+            Timestamp date = bike.getCreationDate() != null ? new Timestamp(
+                    bike.getCreationDate().getTime().getTime()) : null;
+            preparedStatement.setTimestamp(i++, date);
+
+            /* Execute query. */
+            preparedStatement.executeUpdate();
+
+            /* Get generated identifier. */
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+
+            if (!resultSet.next()) {
+                throw new SQLException(
+                        "JDBC driver did not return generated key.");
+            }
+            Long bikeId = resultSet.getLong(1);
+
+            /* Return movie. */
+            return new Bike(bikeId, bike.getDescription(), bike.getPrice(), 
+                    bike.getCreationDate());
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
