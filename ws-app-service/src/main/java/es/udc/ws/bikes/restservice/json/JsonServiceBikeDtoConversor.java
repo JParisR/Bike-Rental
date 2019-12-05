f0f177273941b5042a26b7945b15cb57e35b99a9
Samuel Mouro <s.mourog@udc.es>
2019-12-05 17:20:55 +0100

    JsonServiceBikeDtoConversor

--- ws-app-service/src/main/java/es/udc/ws/bikes/restservice/json/JsonServiceBikeDtoConversor.java
+++ ws-app-service/src/main/java/es/udc/ws/bikes/restservice/json/JsonServiceBikeDtoConversor.java
@@ -1,5 +1,23 @@
 package es.udc.ws.bikes.restservice.json;
 
-public class JsonServiceBikeDtoConversor {
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import es.udc.ws.bikes.dto.ServiceBikeDto;
+
+public class JsonServiceBikeDtoConversor {
+	
+	public static ObjectNode toObjectNode(ServiceBikeDto bike) {
+		ObjectNode bikeObject = JsonNodeFactory.instance.objectNode();
+		
+		if (bike.getBikeId() != null) {
+			bikeObject.put("bikeId", bike.getBikeId());
+		}
+		bikeObject.put("description", bike.getDescription()).
+			put("price", bike.getPrice()).
+			put("units", bike.getUnits());
+		
+		return bikeObject;
+	}
+	
 }
