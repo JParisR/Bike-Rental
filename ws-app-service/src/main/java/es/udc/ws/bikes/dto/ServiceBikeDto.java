1601d778c08e3767a795b6946decc2d6485936be
Samuel Mouro <s.mourog@udc.es>
2019-12-05 11:02:19 +0100

    ServiceBikeDto

--- ws-app-service/src/main/java/es/udc/ws/bikes/dto/ServiceBikeDto.java
+++ ws-app-service/src/main/java/es/udc/ws/bikes/dto/ServiceBikeDto.java
@@ -1,5 +1,62 @@
 package es.udc.ws.bikes.dto;
 
-public class ServiceBikeDto {
+import java.util.Calendar;
 
+public class ServiceBikeDto {
+	
+	private Long bikeId;
+	private String description;
+	private float price;
+	private int units;
+	
+	public ServiceBikeDto() {
+		
+	}
+	
+	public ServiceBikeDto(Long bikeId, String description, float price, int units) {
+		this.bikeId = bikeId;
+		this.description = description;
+		this.price = price;
+		this.units = units;
+	}
+
+	public Long getBikeId() {
+		return bikeId;
+	}
+	
+	public void setBikeId(Long bikeId) {
+		this.bikeId = bikeId;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public float getPrice() {
+		return price;
+	}
+	
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+	public int getUnits() {
+		return units;
+	}
+	
+	public void setUnits(int units) {
+		this.units = units;
+	}
+	
+	@Override
+	public String toString() {
+		return "BikeDto [bikeId=" + bikeId + ", units=" + units
+				+ ", price=" + price
+				+ ", description=" + description +"]";
+	}
 }
+
